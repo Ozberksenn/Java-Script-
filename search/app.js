6a7883e38b08fe8cc8ab33524f70ed9f97413bbf
Özberk Şen <ozberksenn@gmail.com>
2022-04-12 00:37:43 +0300

    Hidden Search

---
+++ search/app.js
@@ -0,0 +1,7 @@
+let search = document.querySelector(".search");
+let input = document.querySelector(".input");
+let btn = document.querySelector(".btn");
+
+btn.addEventListener("mouseenter", () => {
+  search.classList.toggle("active");
+});
